c3911bf7d1ad6018fa9327a4665f83504af244e2
Mikael Aare <aaremikael@gmail.com>
2021-09-15 14:42:27 +0200

    Oppgave 1 done :heavy_check_mark:

--- Obligatoriske oppgaver/Obligatorisk 1/main.hs
+++ Obligatoriske oppgaver/Obligatorisk 1/main.hs
@@ -37,25 +37,22 @@ dictionary = [
 
 
 -- Oppgave 1 ----------------------------------------------------
-isPrefix1 :: String -> String -> Bool 
-isPrefix1 [] ys = True 
-isPrefix1 xs [] = False 
-isPrefix1 (x:xs) (y:ys)
-        | x == y        = isPrefix1 xs ys
+isPrefix :: String -> String -> Bool 
+isPrefix [] ys = True 
+isPrefix xs [] = False 
+isPrefix (x:xs) (y:ys)
+        | x == y        = isPrefix xs ys
         | otherwise     = False
 
-isPrefix2 :: String -> String -> Bool 
-isPrefix2 xs ys = take (length xs) ys == xs
-
-isPrefix1 xs ys | x == y - isPrefix1 (tail xs) (tail ys)
 
 
 -- Oppgave 2 ----------------------------------------------------
-locate :: String -> String -> [(Int, Int)]
+locate :: String -> String -> [(Int,Int)]
 locate [] ys = []
 locate xs [] = []
 
 
+{-
 -- Oppgave 3 ----------------------------------------------------
 translate :: String -> String 
 translate = undefined 
@@ -79,3 +76,4 @@ analytics = undefined
 -- Oppgave 7 ----------------------------------------------------
 main :: String -> (String, Int)
 main = undefined
+-}
