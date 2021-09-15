94998f61c3adc2b0b911803a100b4d7c54138394
Mikael Aare <aaremikael@gmail.com>
2021-09-15 14:12:53 +0200

    Imported skeleton for the task

--- Obligatoriske oppgaver/Obligatorisk 1/main.hs
+++ Obligatoriske oppgaver/Obligatorisk 1/main.hs
@@ -1,4 +1,42 @@
--- Oppgave 1
+-- Mikael Aare
+module Oblig1 where
+
+dictionary = [
+        ("bb",["Big Brother"]),
+        ("dep",["department"]),
+        ("sec", ["Sector"]),
+        ("doubleplusgood",["excellent", "fabulous", "fantastic", "best"]),
+        ("doubleplusungood", ["terrible", "horrible", "worst"]),
+        ("Ingsoc", ["English Socialism"]),
+        ("joycamp", ["labour camp"]),
+        ("Oldspeak", ["Standard English", "English"]),
+        ("oldthink", ["objectivity", "rationalism", "democracy"]),
+        ("thinkpol", ["The Thought Police"]),
+        ("prolefeed", ["Popular culture", "pop-culture"]),
+        ("crimethink", ["liberty", "equality", "privacy", "thoughtcrime"]),
+        ("fullwise", ["fully", "completely", "totally"]),
+        ("goodthink", ["political orthodoxy", "politically orthodox thought", "orthodox thought"]),
+        ("goodwise", ["well"]),
+        ("ownlife", ["anti-social tendency", "solitude", "individualism"]),
+        ("plusgood", ["very good", "great"]),
+        ("plusungood", ["very bad"]),
+        ("misprint", ["error", "misprediction"]),
+        ("Miniluv", ["The Ministry of Love"]),
+        ("Minipax", ["The Ministry of Peace"]),
+        ("Minitrue", ["The Ministry of Truth"]),
+        ("Miniplenty", ["The Ministry of Plenty"]),
+        ("bellyfeel", ["blind, enthusiastic acceptance"]),
+        ("doublethink", ["believing two contradictory ideas"]),
+        ("duckspeak", ["vocal support of political orthodoxies"]),
+        ("un", ["not"]),
+        ("peace", ["war"]),
+        ("strength", ["ignorance"]),
+        -- The next line contains a list of forbidden words that don't have a translation to Newspeak, these should be replaced with '*'s
+        ("",["freedom", "revolution", "fun", "diary", "surveillance", "Great Britain", "Winston Smith", "Julia"])
+        ]
+
+
+-- Oppgave 1 ----------------------------------------------------
 isPrefix1 :: String -> String -> Bool 
 isPrefix1 [] ys = True 
 isPrefix1 xs [] = False 
@@ -12,22 +50,32 @@ isPrefix2 xs ys = take (length xs) ys == xs
 isPrefix1 xs ys | x == y - isPrefix1 (tail xs) (tail ys)
 
 
-
--- Oppgave 2
+-- Oppgave 2 ----------------------------------------------------
 locate :: String -> String -> [(Int, Int)]
 locate [] ys = []
 locate xs [] = []
 
 
+-- Oppgave 3 ----------------------------------------------------
+translate :: String -> String 
+translate = undefined 
 
 
-
--- Oppgave 3
-translate :: String -> String
-translate xs =
-
-
-
--- Oppgave 4
-replace :: [(Int, Int)] -> String -> String 
+-- Oppgave 4 ----------------------------------------------------
+replace :: [(Int,Int)] -> String -> String 
 replace = undefined
+
+
+-- Oppgave 5 ----------------------------------------------------
+toNewspeak :: String -> String 
+toNewspeak = undefined
+
+
+-- Oppgave 6 ----------------------------------------------------
+analytics :: String -> String -> Int 
+analytics = undefined
+
+
+-- Oppgave 7 ----------------------------------------------------
+main :: String -> (String, Int)
+main = undefined
